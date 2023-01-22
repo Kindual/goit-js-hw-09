96849cc6025eeb6d26c13d82ddfe4e3d6ff018c5
Kindual <110603179+Kindual@users.noreply.github.com>
2023-01-22 05:48:51 +0200

    color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const bodyEl = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+let timerToSwitchColor = null;
+let switching = false;
+
+
+btnStart.addEventListener('click', () => {
+    if(switching) {
+        return;
+    }
+
+    timerToSwitchColor = setInterval(colorSwitch, 1000);
+    switching = true;
+
+    console.log('switcher start');
+});
+
+btnStop.addEventListener('click', () => {
+    if(switching === false) {
+        return;
+    }
+
+    clearInterval(timerToSwitchColor);
+    switching = false;
+
+    console.log('switcher stopped');
+})
+
+function colorSwitch() {
+    bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
